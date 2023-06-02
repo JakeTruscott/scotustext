10fcc8509ad121518991f2348327afe8fb76ed0e
Jake Truscott <jake.truscott44@gmail.com>
2023-06-02 16:06:18 -0400

    Updating Decision Processor

--- R/decision_cleaner.R
+++ R/decision_cleaner.R
@@ -1,45 +1,27 @@
-decision_cleaner <- function(dir_path){
+decisions_cleaner <- function(file_path, dir_path) {
 
   {
-    directory <- trimws(dir_path)
-    file_directory <- paste0(directory, "/")
-    file_names <- list.files(file_directory)
-    total_files <- length(file_names)
-    dir_path <- trimws(dir_path)
-    dir_path <- paste0(dir_path, "/*")
-    rtext <- readtext::readtext(dir_path)
-  } #File Path
-
-  {
+    rtext <- readtext::readtext(file_path)
     corpus <- Corpus(VectorSource(rtext$text))
     corpus <- tm_map(corpus, content_transformer(iconv), to = "ASCII//TRANSLIT")
-    corpus <- tm_map(corpus, content_transformer(gsub), pattern = "on writ of certiorari", replacement = "<DECISION BREAK> on writ of certiorari", ignore.case = T)
-    corpus <- tm_map(corpus, content_transformer(gsub), pattern = "on petition for writ of certiorari", replacement = "<DECISION BREAK> on writ of certiorari", ignore.case = T)
-    corpus <- tm_map(corpus, content_transformer(gsub), pattern = "on writ of certiorari", replacement = "<DECISION BREAK> <KEEP>", ignore.case = T)
-    corpus <- tm_map(corpus, content_transformer(gsub), pattern = "on petition for writ of certiorari", replacement = "<DECISION BREAK> <KEEP>", ignore.case = T)
-    corpus <- tm_map(corpus, content_transformer(gsub), pattern = "certiorari to the (United States|Court|Supreme)", replacement = "<DECISION BREAK> <KEEP>", ignore.case = T)
-    corpus <- tm_map(corpus, content_transformer(gsub), pattern = "appeal from the united states district court", replacement = "<DECISION BREAK> <KEEP>", ignore.case = T)
-    corpus <- tm_map(corpus, content_transformer(gsub), pattern = "on bill of complaint", replacement = "<DECISION BREAK> <KEEP>", ignore.case = T)
-    decisions <- data.frame(text = sapply(corpus, as.character), stringsAsFactors = FALSE) # Convert Corpus to Dataframe
-
-  } #Corpus Transformation
-
+    corpus <- tm_map(corpus, content_transformer(gsub), pattern = "on writ of certiorari", replacement = "<DECISION BREAK> on writ of certiorari", ignore.case = TRUE)
+    corpus <- tm_map(corpus, content_transformer(gsub), pattern = "on petition for writ of certiorari", replacement = "<DECISION BREAK> on writ of certiorari", ignore.case = TRUE)
+    corpus <- tm_map(corpus, content_transformer(gsub), pattern = "on writ of certiorari", replacement = "<DECISION BREAK> <KEEP>", ignore.case = TRUE)
+    corpus <- tm_map(corpus, content_transformer(gsub), pattern = "on petition for writ of certiorari", replacement = "<DECISION BREAK> <KEEP>", ignore.case = TRUE)
+    corpus <- tm_map(corpus, content_transformer(gsub), pattern = "certiorari to the (United States|Court|Supreme)", replacement = "<DECISION BREAK> <KEEP>", ignore.case = TRUE)
+    corpus <- tm_map(corpus, content_transformer(gsub), pattern = "appeal from the united states district court", replacement = "<DECISION BREAK> <KEEP>", ignore.case = TRUE)
+    corpus <- tm_map(corpus, content_transformer(gsub), pattern = "on bill of complaint", replacement = "<DECISION BREAK> <KEEP>", ignore.case = TRUE)
+    decisions <- data.frame(text = sapply(corpus, as.character), stringsAsFactors = FALSE)
+  } #Collect and Pre-Process Text
   {
-    file_names <- list.files(directory)
-    file_names_processed <- gsub("\\_.*", "", file_names)
+    file_names_processed <- gsub(paste0(dir_path, "/"), "", file_path)
+    file_names_processed <- gsub("\\_.*", "", file_names_processed)
     file_names_processed <- gsub(".pdf", "", file_names_processed)
-    file_names_processed <- data.frame(file_names_processed)
-    names(file_names_processed)[1] <- "file_names"
-    decisions$argument <- file_names_processed$file_names
+    decisions$argument <- file_names_processed
 
-    # Specify the path to the folder containing the PDF files
-    folder_path <- "C:/Users/Jake Truscott/OneDrive - University of Georgia/Active/Justices Oral Argument Rhetoric/SCOTUS_Transcripts/decision_sample"
-
-    pdf_files <- list.files(directory, pattern = "\\.pdf$", full.names = TRUE)
     case_names <- c()
 
-    for (pdf_file in pdf_files) {
-      # Extract metadata from the PDF
+    for (pdf_file in file_path) {
       pdf_metadata <- pdf_info(pdf_file)
       title <- pdf_metadata$keys$Title
       case_names <- c(case_names, title)
@@ -55,12 +37,13 @@ decision_cleaner <- function(dir_path){
     })
     case_names$published <- sapply(case_names$case_names, function(x) {
       if (grepl("\\(", x)) {
-        return(sub(".*\\(|\\).*", "", x))
+        x <- sub(".*\\(|\\).*", "", x)
+        x <- paste0("(", x)
+        return(x)
       } else {
         return(NA)
       }
     })
-    case_names$published <- paste0("(", case_names$published)
 
     case_names$case_names <- mapply(function(case_name, published, docket_number) {
       if (!is.na(published)) {
@@ -75,19 +58,13 @@ decision_cleaner <- function(dir_path){
 
 
 
-    case_names$case_names <- gsub(" \\(\\)", "", case_names$case_names)
-    case_names$case_names <- trimws(case_names$case_names)
-
-    decisions$case_names <- case_names$case_names
-    decisions$published <- case_names$published
   } #File Metadata
-
   {
     decisions_test <- decisions %>%
-      group_by(argument) %>%
       mutate(text = str_split(text, "\\<DECISION BREAK\\>")) %>%
       unnest(text) %>%
-      filter(grepl("on writ of certiorari", text, ignore.case = TRUE)) %>%
+      filter(grepl("\\<Keep\\>", text, ignore.case = TRUE)) %>%
+      filter(!grepl("syllabus\\n\\n", text, ignore.case = TRUE)) %>%
       mutate(text = trimws(text)) %>%
       mutate(text = gsub("\\n\\n", " \n\n ", text, perl = TRUE)) %>%
       mutate(text = gsub("\\n\\n ------\\n[ ]*", "<BEGIN FOOTNOTE> ", text)) %>%
@@ -113,8 +90,9 @@ decision_cleaner <- function(dir_path){
       mutate(footnotes = gsub("(?<![A-Za-z0-9\\n])\\s{5,}(?![A-Za-z0-9\\n])", " ", footnotes, perl = TRUE)) %>%
       mutate(footnotes = gsub("^\\s+", "", footnotes)) %>%
       mutate(footnotes = gsub("(?<=\\n)(\\d+)", "[\\1]", footnotes, perl = TRUE)) %>%
-      mutate(footnotes = gsub("\\n\\n(?=\\[\\d{4,}\\])", "", footnotes, perl = TRUE))%>%
+      mutate(footnotes = gsub("\\n\\n(?=\\[\\d{4,}\\])", "", footnotes, perl = TRUE)) %>%
       mutate(footnotes = str_replace(footnotes, "^(\\d+)", "[\\1]")) %>%
+      mutate(footnotes = gsub("\\- ", "", footnotes)) %>%
       mutate(text = gsub("\\n(?=\\d)", " [", text, perl = TRUE)) %>%
       mutate(text = gsub("\\s(?=\\d)", "] ", text, perl = TRUE)) %>%
       mutate(text = gsub("\\s(?=\\n)", "] ", text, perl = TRUE)) %>%
@@ -145,6 +123,6 @@ decision_cleaner <- function(dir_path){
       mutate(text = gsub("-\\n", "", text)) %>%
       mutate(text = gsub("\\n", " ", text)) %>%
       select(-c(opinion))
-  } #Process Opinions
+  } #Process & Clean Docket Frame
 
-}
+} #Process Opinions
--- R/decision_processor.R
+++ R/decision_processor.R
@@ -1,17 +1,155 @@
-decision_processor <- function(dir_path){
-  cat("\nDecisions to Process: ", length(list.files(dir_path)))
+decision_processor <- function(dir_path) {
+  files <- list.files(dir_path, full.names = TRUE)
+  num_files <- length(files)
+  cat("\nDecisions to Process: ", num_files)
+  all_decisions <- NULL
+
   start_time <- Sys.time()
-  cleaned_decisions <- suppressWarnings(decision_cleaner(dir_path = dir_path))
+  for (i in files) {
+    cleaned_decisions <- suppressWarnings(decisions_cleaner(file_path = i, dir_path = i))
+    all_decisions <- rbind(all_decisions, cleaned_decisions)
+  }
+
   end_time <- Sys.time()
   elapsed_time <- end_time - start_time
+
   cat("\nSuccess!")
   cat("\n - - - - - - - Summary - - - - - - - ")
   cat("\nCompletion Time: ", round(as.numeric(elapsed_time), 2), "Seconds")
-  cat("\nNumber of Unique Opinions: ", length(cleaned_decisions$argument))
-  cat("\nNumber of Majority Opinions", length(cleaned_decisions$opinion_type[cleaned_decisions$opinion_type == "Majority Opinion"]))
-  cat("\nNumber of Dissents:", length(cleaned_decisions$opinion_type[cleaned_decisions$opinion_type == "Dissent"]))
-  cat("\nNumber of Concurrences:", length(cleaned_decisions$opinion_type[cleaned_decisions$opinion_type == "Concurrence"]))
-  cat("\nNumber of Per Curiam:", length(cleaned_decisions$opinion_type[cleaned_decisions$opinion_type == "Per Curiam"]))
+  #cat("\nNumber of Unique Opinions: ", length(unique(all_decisions$argument)))
+  cat("\nNumber of Majority Opinions: ", sum(all_decisions$opinion_type == "Majority Opinion"))
+  cat("\nNumber of Dissents: ", sum(all_decisions$opinion_type == "Dissent"))
+  cat("\nNumber of Concurrences: ", sum(all_decisions$opinion_type == "Concurrence"))
+  cat("\nNumber of Per Curiam: ", sum(all_decisions$opinion_type == "Per Curiam"))
 
-  return(cleaned_decisions)
+  return(all_decisions)
 }
+
+decisions_cleaner <- function(file_path, dir_path) {
+
+  {
+    rtext <- readtext::readtext(file_path)
+    corpus <- Corpus(VectorSource(rtext$text))
+    corpus <- tm_map(corpus, content_transformer(iconv), to = "ASCII//TRANSLIT")
+    corpus <- tm_map(corpus, content_transformer(gsub), pattern = "on writ of certiorari", replacement = "<DECISION BREAK> on writ of certiorari", ignore.case = TRUE)
+    corpus <- tm_map(corpus, content_transformer(gsub), pattern = "on petition for writ of certiorari", replacement = "<DECISION BREAK> on writ of certiorari", ignore.case = TRUE)
+    corpus <- tm_map(corpus, content_transformer(gsub), pattern = "on writ of certiorari", replacement = "<DECISION BREAK> <KEEP>", ignore.case = TRUE)
+    corpus <- tm_map(corpus, content_transformer(gsub), pattern = "on petition for writ of certiorari", replacement = "<DECISION BREAK> <KEEP>", ignore.case = TRUE)
+    corpus <- tm_map(corpus, content_transformer(gsub), pattern = "certiorari to the (United States|Court|Supreme)", replacement = "<DECISION BREAK> <KEEP>", ignore.case = TRUE)
+    corpus <- tm_map(corpus, content_transformer(gsub), pattern = "appeal from the united states district court", replacement = "<DECISION BREAK> <KEEP>", ignore.case = TRUE)
+    corpus <- tm_map(corpus, content_transformer(gsub), pattern = "on bill of complaint", replacement = "<DECISION BREAK> <KEEP>", ignore.case = TRUE)
+    decisions <- data.frame(text = sapply(corpus, as.character), stringsAsFactors = FALSE)
+  } #Collect and Pre-Process Text
+  {
+    file_names_processed <- gsub(paste0(dir_path, "/"), "", file_path)
+    file_names_processed <- gsub("\\_.*", "", file_names_processed)
+    file_names_processed <- gsub(".pdf", "", file_names_processed)
+    decisions$argument <- file_names_processed
+
+    case_names <- c()
+
+    for (pdf_file in file_path) {
+      pdf_metadata <- pdf_info(pdf_file)
+      title <- pdf_metadata$keys$Title
+      case_names <- c(case_names, title)
+    }
+
+    case_names <- data.frame(case_names)
+    case_names$docket_number <- sapply(case_names$case_names, function(x) {
+      if (grepl(" ", x)) {
+        return(sub(" .*", "", x))
+      } else {
+        return(NA)
+      }
+    })
+    case_names$published <- sapply(case_names$case_names, function(x) {
+      if (grepl("\\(", x)) {
+        x <- sub(".*\\(|\\).*", "", x)
+        x <- paste0("(", x)
+        return(x)
+      } else {
+        return(NA)
+      }
+    })
+
+    case_names$case_names <- mapply(function(case_name, published, docket_number) {
+      if (!is.na(published)) {
+        case_name <- gsub(published, "", case_name, fixed = TRUE)
+      }
+      if (!is.na(docket_number)) {
+        case_name <- gsub(docket_number, "", case_name, fixed = TRUE)
+      }
+      return(case_name)
+    }, case_names$case_names, case_names$published, case_names$docket_number)
+
+
+
+
+  } #File Metadata
+  {
+    decisions_test <- decisions %>%
+      mutate(text = str_split(text, "\\<DECISION BREAK\\>")) %>%
+      unnest(text) %>%
+      filter(grepl("\\<Keep\\>", text, ignore.case = TRUE)) %>%
+      filter(!grepl("syllabus\\n\\n", text, ignore.case = TRUE)) %>%
+      mutate(text = trimws(text)) %>%
+      mutate(text = gsub("\\n\\n", " \n\n ", text, perl = TRUE)) %>%
+      mutate(text = gsub("\\n\\n ------\\n[ ]*", "<BEGIN FOOTNOTE> ", text)) %>%
+      mutate(text = gsub("\\n------\\n", " \n------\n ", text, perl = TRUE)) %>%
+      mutate(text = gsub("\\n------\\n", " <BEGIN FOOTNOTE> ", text, perl = TRUE)) %>%
+      mutate(text = str_replace_all(text, "J., dissenting\\s+\\n\\n", "J., dissenting \n\n <END HEADER> ")) %>%
+      mutate(text = str_replace_all(text, "J., concurring\\s+\\n\\n", "J., dissenting \n\n <END HEADER> ")) %>%
+      mutate(text = str_replace_all(text, "Opinion of the Court\\s+\\n\\n", "J., dissenting \n\n <END HEADER> ")) %>%
+      mutate(text = str_replace_all(text, "\\n\\n\\s+\\d+", "<BEGIN HEADER>")) %>%
+      mutate(text = str_replace_all(text, "Cite as:", " <BEGIN HEADER> Cite as:"))  %>%
+      mutate(footnotes = text) %>%
+      mutate(footnotes = str_extract_all(text, "(?<=<BEGIN FOOTNOTE>)([\\s\\S]*?)(?=<BEGIN HEADER>)")) %>%
+      mutate(footnotes = sapply(footnotes, function(x) if (length(x) > 0) paste(x, collapse = "\n\n") else NA_character_)) %>%
+      mutate(text = gsub("<BEGIN FOOTNOTE>.*?<END HEADER>", "", text)) %>%
+      mutate(text = gsub("<BEGIN HEADER>.*?<END HEADER>", "", text)) %>%
+      mutate(footnotes = gsub("\\n\\n {3,}(\\d+)", "<FOOTNOTE BREAK> \\1", footnotes)) %>%
+      mutate(footnotes = gsub("\\n\\n", "", footnotes)) %>%
+      mutate(footnotes = gsub("<FOOTNOTE BREAK>", "\n\n", footnotes)) %>%
+      mutate(footnotes = gsub("\\-  ", "", footnotes)) %>%
+      mutate(footnotes = gsub("\\- ", "", footnotes)) %>%
+      mutate(footnotes = gsub("\\n", " ", footnotes)) %>%
+      mutate(footnotes = gsub("  (\\d+)", "\n\n\\1", footnotes)) %>%
+      mutate(footnotes = gsub("(?<![A-Za-z0-9\\n])\\s{5,}(?![A-Za-z0-9\\n])", " ", footnotes, perl = TRUE)) %>%
+      mutate(footnotes = gsub("^\\s+", "", footnotes)) %>%
+      mutate(footnotes = gsub("(?<=\\n)(\\d+)", "[\\1]", footnotes, perl = TRUE)) %>%
+      mutate(footnotes = gsub("\\n\\n(?=\\[\\d{4,}\\])", "", footnotes, perl = TRUE)) %>%
+      mutate(footnotes = str_replace(footnotes, "^(\\d+)", "[\\1]")) %>%
+      mutate(footnotes = gsub("\\- ", "", footnotes)) %>%
+      mutate(text = gsub("\\n(?=\\d)", " [", text, perl = TRUE)) %>%
+      mutate(text = gsub("\\s(?=\\d)", "] ", text, perl = TRUE)) %>%
+      mutate(text = gsub("\\s(?=\\n)", "] ", text, perl = TRUE)) %>%
+      mutate(text = gsub("\\[(?=\\d)", "", text, perl = TRUE)) %>%
+      mutate(text = gsub("\\](?=\\n)", "", text, perl = TRUE)) %>%
+      mutate(text = gsub("\\](?=\\s)", "", text, perl = TRUE)) %>%
+      mutate(opinion = sub("\\.(.*)", "", text)) %>%
+      mutate(opinion = sub("\\.(.*)", "", text)) %>%
+      mutate(opinion = trimws(opinion) %>%
+               paste0(".")) %>%
+      mutate(opinion_writer = str_extract_all(opinion, "(CHIEF JUSTICE|JUSTICE)\\s+(\\w+)") %>%
+               sapply(paste, collapse = "; ") %>%
+               sapply(function(x) gsub(" joins| join", "", x))) %>%
+      mutate(opinion_type = case_when(
+        grepl("delivered", opinion, ignore.case = T) & grepl("court", opinion, ignore.case = T) ~ "Majority Opinion",
+        grepl("dissenting", opinion) ~ "Dissent",
+        grepl("concurring", opinion) ~ "Concurrence",
+        grepl("concurrence", opinion) & grepl("dissenting", opinion) & grepl("in part", opinion) ~ "Concur & Dissent (In Part)")) %>%
+      mutate(opinion_type = ifelse(is.na(opinion_type), "Per Curiam", opinion_type))  %>%
+      mutate(text = ifelse(opinion_type == "Per Curiam", gsub(".*PER CURIAM", "", text), text)) %>%
+      mutate(opinion_writer = ifelse(opinion_type == "Per Curiam", "Per Curiam", opinion_writer)) %>%
+      mutate(text = sub(".*?\\.", "", text)) %>%
+      mutate(text = trimws(text)) %>%
+      mutate(text = gsub("U\\.\\sS\\.\\sC\\.\\s\\?", "U. S. C. \u00A7", text)) %>%
+      mutate(text = gsub("\\?(?=\\d)", "\u00A7", text, perl = T)) %>%
+      mutate(text = gsub("\\s{2,}", " ", text)) %>%
+      mutate(text = gsub("SUPREME COURT OF THE UNITED STATES.*", "", text)) %>%
+      mutate(text = gsub("-\\n", "", text)) %>%
+      mutate(text = gsub("\\n", " ", text)) %>%
+      select(-c(opinion))
+  } #Process & Clean Docket Frame
+
+} #Process Opinions
